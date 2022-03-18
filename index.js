7eaf0e7243987bdcff94f83254f66f58dbbd2eec
Victorsousa98 <victorsousa708@gmail.com>
2022-03-18 16:17:05 -0300

    refatoração com .then

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code, "não há arquivo no caminho"))
 }
 
-function pegaArquivo(arquivo){
-    const encoding = 'utf-8'
-    fs.readFile(arquivo, encoding, (erro,texto) => {
-        if(erro){
-            trataErro(erro)
-        }else{
-            console.log(chalk.green(texto))
-        }
-    })
-}
+
+// function pegaArquivo(arquivo){
+//     fs.promises.readFile(arquivo, "utf-8")
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch((erro) => trataErro(erro))
+// }
+
+
+// function pegaArquivo(arquivo){
+//     const encoding = 'utf-8'
+//     fs.readFile(arquivo, encoding, (erro,texto) => {
+//         if(erro){
+//             trataErro(erro)
+//         }else{
+//             console.log(chalk.green(texto))
+//         }
+//     })
+// }
 
 pegaArquivo("./arquivo/text.md")
