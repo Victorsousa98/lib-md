8ce7bf3ed04b776b44c5818d2ac1b0e49a36a0e6
Victorsousa98 <victorsousa708@gmail.com>
2022-03-24 23:11:33 -0300

    adiciona cli e validação

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import chalk from "chalk"
 import fs from "fs"
 
-function trataErro(erro){
-    throw new Error(chalk.red(erro.code, "não há arquivo no caminho"))
-}
-async function pegaArquivo(arquivo){
-    try {
-        const texto = await fs.promises.readFile(arquivo, "utf-8")
-        console.log(chalk.green(texto))
-    } catch(erro){
-        trataErro(erro)
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null) {
+      arrayResultados.push({ [temp[1]]: temp[2] })
     }
-}
+    return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
+  }
+  
+  function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+  }
+  
+  export default async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
+      const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+      return extraiLinks(texto);
+    } catch(erro) {
+      trataErro(erro);
+    }
+  }
+  
+  //pegaArquivo('./arquivo/text.md');
+
 
 // function pegaArquivo(arquivo){
 //     fs.promises.readFile(arquivo, "utf-8")
@@ -29,6 +44,4 @@ async function pegaArquivo(arquivo){
 //             console.log(chalk.green(texto))
 //         }
 //     })
-// }
-
-pegaArquivo("./arquivo/text.md")
+// 
